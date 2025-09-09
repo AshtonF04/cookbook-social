54910466e86aa2b9939c24112a71620f5973b609
Ashton Franklin <ashtonfranklin772@gmail.com>
2025-09-09 19:02:07 -0400

    Setup react environment with tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Aj is a Faggot</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
